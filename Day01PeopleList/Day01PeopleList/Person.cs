eb53f519d8f1dd656719d6b579157ed3c24e1ea0
Mer-W <whmere@gmail.com>
2023-10-25 02:37:24 +0000

    initial

---
+++ Day01PeopleList/Day01PeopleList/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01PeopleList
+{
+    public class Person
+    {
+        public string Name; // Name 2-100 characters long, not containing semicolons
+        public int Age; // Age 0-150
+        public string City; // City 2-100 characters long, not containing semicolons
+    }
+}
---
+++ Day01PeopleList/Day01PeopleList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01PeopleList
+{
+    internal class Program
+    {
+        static List<Person> people = new List<Person>();
+        static void AddPersonInfo()
+        {
+
+        }
+static void ListAllPersonsInfo()
+        {
+
+        }
+static void FindPersonByName()
+        {
+
+        }
+static void FindPersonYoungerThan()
+        {
+
+        }
+        static void Main(string[] args)
+
+        {
+
+        }
+        static void ReadAllPeopleFromFile()
+        {
+
+        }
+static void SaveAllPeopleToFile()
+        {
+
+        }
+
+
+    }
+}
